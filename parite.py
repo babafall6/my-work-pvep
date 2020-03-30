98770b0fde90ba498c3cc2d594306424b7443d6f
Papa Baba Fall <pbfall@sankofaimpact.sn>
2020-03-30 16:42:15 +0200

    Manage errors and bugs using module logging and bloc try ...

--- parite.py
+++ parite.py
@@ -1,9 +1,12 @@
 #! /usr/bin/python3
 # coding: utf-8
 
+import logging as lg
 import argparse
 import analysis.csv as c_an
 import analysis.xml as x_an
+
+lg.basicConfig(level=lg.DEBUG)
 
 def parse_arguments():
 	parser = argparse.ArgumentParser()
@@ -15,13 +18,23 @@ def parse_arguments():
 
 if __name__ == "__main__":
 	args = parse_arguments()
-	datafile = args.datafile
-	if datafile :
-		if args.extension == 'xml':
-			#'xml/compteRendu/CRSANR5L15S2017E1N001.xml'
-			x_an.launch_analysis()
-		elif args.extension == 'csv':
-			#'current_mps.csv'
-			c_an.launch_analysis(datafile) 
-	else:
-		print("Please give filename CSV")
+	try:
+		datafile = args.datafile
+		if not datafile :
+			raise Warning('You must indicate a datafile !')
+		else:	
+			try:
+
+				if args.extension == 'xml':
+					#'xml/compteRendu/CRSANR5L15S2017E1N001.xml'
+					x_an.launch_analysis(datafile)
+				elif args.extension == 'csv':
+					#'current_mps.csv'
+					c_an.launch_analysis(datafile) 
+
+			except FileNotFoundError as e :
+				lg.critical("Ow :( The file was not found. \nHere is the original message of the exception : {}".format(e))
+			finally:
+				lg.info('#'*10 + ' Analysis is over ' + '#'*10)
+	except Warning as e:
+		lg.warning(e)
